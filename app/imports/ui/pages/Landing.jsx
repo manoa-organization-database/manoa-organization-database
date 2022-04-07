720c745da8809ba3105c7c636045c8a7a17780cf
msumaylo <97885062+msumaylo@users.noreply.github.com>
2022-04-07 13:38:36 -1000

    Issue #1: initial version of landing page finished.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Container, Header } from 'semantic-ui-react';
+import { Grid, Image, Container, Header, Divider } from 'semantic-ui-react';
 
 /** Renders a color-blocked static landing page. */
 class Landing extends React.Component {
@@ -9,47 +9,49 @@ class Landing extends React.Component {
         <div className='landing-green-background'>
           <Container textAlign='center'>
             <Header style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }} as='h1'>
-                Welcome to Bowfolios
+              Manoa Organization Database
             </Header>
             <Header style={{ paddingBottom: '20px', color: 'white' }} as='h3'>
-                Profiles, projects, and interest areas for the UH Community
+              A catalog for the various student clubs of UH Manoa
             </Header>
           </Container>
         </div>
         <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Start by making your profile....</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/home-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/profiles-page.png"/>
-            </Grid.Column>
-          </Grid>
+          <Divider horizontal>
+            <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Club Officers</Header>
+          </Divider>
+          <Header as="h3" style={{ color: '#376551' }} textAlign="center">Showcase your clubs by providing descriptions,
+            location and contact information, and other promotional information.</Header>
+          <Container>
+            <Image src="/images/club-admin-home-page.png" bordered alt="Club admin home page"/>
+          </Container>
+          <Divider horizontal hidden section/>
         </div>
         <div className='landing-green-background'>
-          <Header style={{ color: 'white' }} as='h2' textAlign='center'>...then add your projects</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/add-project-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/projects-page.png"/>
-            </Grid.Column>
-          </Grid>
+          <Divider horizontal>
+            <Header style={{ color: 'white' }} as='h2' textAlign='center'>Students</Header>
+          </Divider>
+          <Header style={{ color: 'white' }} as='h3' textAlign='center'>Create a profile and highlight your interests.</Header>
+          <Container>
+            <Image src="/images/user-home-page-mockup.png" bordered/>
+          </Container>
+          <Header style={{ color: 'white' }} as='h3' textAlign='center'>Search for clubs based on interests,
+            and get notifications when clubs related to your interest are created.</Header>
+          <Container>
+            <Image src="/images/browse-clubs-page-mockup.png" bordered/>
+          </Container>
+          <Divider horizontal hidden section />
         </div>
         <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>
-              Connect to people and projects with shared interests!
-          </Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/interests-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/filter-page.png"/>
-            </Grid.Column>
-          </Grid>
+          <Divider horizontal>
+            <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Admin Users</Header>
+          </Divider>
+          <Header as="h3" style={{ color: '#376551' }} textAlign="center">Admins may grant club officers club admin powers,
+            and can add and remove new interests for club admins and users to use.</Header>
+          <Container>
+            <Image src="/images/admin-home-page-mockup.png" bordered alt="Admin home page"/>
+          </Container>
+          <Divider horizontal hidden section/>
         </div>
 
       </div>

bcc621988c8a7dbb65ba74d52819ac8b9603c57a
sumidaca <sumidaca@hawaii.edu>
2022-04-11 22:37:02 -1000

    Issue #2 added card component and sample data

--- app/imports/ui/pages/ClubAdminHome.jsx
+++ app/imports/ui/pages/ClubAdminHome.jsx
@@ -1,5 +1,64 @@
 import React from 'react';
 import { Image, Container, Header, Button, Label, Card, Divider } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { _ } from 'meteor/underscore';
+
+const sampleMemberData = [
+  { firstName: 'Club', lastName: 'Admin', email: 'clubadmin@foo' +
+      '.com', picture: 'https://react.semantic-ui.com/images/avatar/large/matthew.png', role: 'Club Admin' },
+  { firstName: 'Matthew', lastName: 'Smith', email: 'matthew@foo' +
+      '.com', picture: 'https://react.semantic-ui.com/images/avatar/large/matthew.png', role: 'Member' },
+  { firstName: 'David', lastName: 'Tennent', email: 'david@foo' +
+      '.com', picture: 'https://react.semantic-ui.com/images/avatar/large/matthew.png', role: 'Member' }];
+
+const sampleInterests = [
+  { email: 'clubadmin@foo.com', interest: 'Clubs' },
+  { email: 'clubadmin@foo.com', interest: 'Mockups' },
+  { email: 'matthew@foo.com', interest: 'Clubs' },
+  { email: 'matthew@foo.com', interest: 'Mockups' },
+  { email: 'david@foo.com', interest: 'Clubs' },
+  { email: 'david@foo.com', interest: 'Mockups' },
+  { email: 'david@foo.com', interest: 'Sports' },
+];
+
+function getMemberData(email) {
+  const data = _.find(sampleMemberData, (member) => member.email === email);
+  const interests = _.pluck(_.filter(sampleInterests, (interest) => interest.email === email), 'interest');
+  return _.extend({ }, data, { interests });
+}
+
+const ClubCard = (props) => (
+  <Card>
+    <Image src={props.member.picture} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{props.member.firstName} {props.member.lastName}</Card.Header>
+      <Card.Meta>
+        <span className='email'>{props.member.email}</span>
+      </Card.Meta>
+      <Card.Meta>
+        <span className='role'>{props.member.role}</span>
+      </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <div>
+        <p>Interests:</p>
+        {_.map(props.member.interests, (interest, index) => <Label key={index} className="club-admin-label">{interest}</Label>)}
+      </div>
+    </Card.Content>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+ClubCard.propTypes = {
+  member: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    email: PropTypes.string,
+    picture: PropTypes.string,
+    role: PropTypes.string,
+    interests: PropTypes.array,
+  }).isRequired,
+};
 
 /** Renders a color-blocked static ClubAdminHome page. */
 class ClubAdminHome extends React.Component {
@@ -7,10 +66,14 @@ class ClubAdminHome extends React.Component {
     name: 'Mockup Club',
     description: 'Do you like making mockups of clubs? Then this is the club for you. The Mockup Club focuses on\n' +
       '              teaching members how to create their own mockups of clubs. Activites include coming up with clubs ideas and\n' +
-      '              writing their own club descriptions such as this one.',
+      '              writing their own club descriptions such as this one. Contact a club admin for details.',
+    picture: 'https://react.semantic-ui.com/images/wireframe/image.png',
+    clubAdmins: 'admin@foo.com',
   };
 
   render() {
+    const emails = ['clubadmin@foo.com', 'matthew@foo.com', 'david@foo.com'];
+    const memberData = emails.map(email => getMemberData(email));
     return (
       <div>
         <div className="club-admin-margin">
@@ -21,20 +84,17 @@ class ClubAdminHome extends React.Component {
         <div className="club-admin-margin">
           <Container textAlign='center'>
             <Divider />
-            <Header as="h1">Mockup Club</Header>
+            <Header as="h1">{this.sampleClub.name}</Header>
             <Divider />
           </Container>
         </div>
         <div className="club-admin-margin">
-          <Image src="https://react.semantic-ui.com/images/wireframe/image.png" size='large' centered/>
+          <Image src={this.sampleClub.picture} size='large' centered/>
         </div>
         <div className="club-admin-margin">
           <Container textAlign='center'>
             <Divider />
-            <Header as="h3">Do you like making mockups of clubs? Then this is the club for you. The Mockup Club focuses on
-              teaching members how to create their own mockups of clubs. Activites include coming up with clubs ideas and
-              writing their own club descriptions such as this one.</Header>
-            <p>Location: POST 319</p>
+            <Header as="h3">{this.sampleClub.description}</Header>
             <Divider />
           </Container>
         </div>
@@ -52,7 +112,8 @@ class ClubAdminHome extends React.Component {
           <Container textAlign="center">
             <Header as="h1">Members</Header>
             <Card.Group centered>
-              <Card>
+              {_.map(memberData, (profile, index) => <ClubCard key={index} member={profile}/>)}
+              { /* <Card>
                 <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
                 <Card.Content>
                   <Card.Header>Admin</Card.Header>
@@ -109,15 +170,9 @@ class ClubAdminHome extends React.Component {
                     <Label className="club-admin-label">Sports</Label>
                   </div>
                 </Card.Content>
-              </Card>
+              </Card> */}
             </Card.Group>
             <Divider />
-          </Container>
-        </div>
-        <div className="club-admin-margin">
-          <Container textAlign="center">
-            <Header as="h1">Contact Info</Header>
-            <p>Email: clubadmin@foo.com</p>
           </Container>
         </div>
       </div>

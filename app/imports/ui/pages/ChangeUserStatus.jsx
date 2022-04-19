6b8f16a33bd0495ec4faaa39b0c14bc85066281c
jeremiah-dy <97951461+jeremiah-dy@users.noreply.github.com>
2022-04-19 11:31:41 -1000

    Update ChangeUserStatus.jsx

--- app/imports/ui/pages/ChangeUserStatus.jsx
+++ app/imports/ui/pages/ChangeUserStatus.jsx
@@ -40,12 +40,12 @@ class ChangeUserStatus extends React.Component {
     });
   }
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
+  } */
 
-  renderPage() {
+  render() {
     const formSchema = makeSchema;
     const bridge = new SimpleSchema2Bridge(formSchema);
     return (
